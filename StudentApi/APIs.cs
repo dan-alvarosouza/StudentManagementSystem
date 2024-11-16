302d6bbcde3c9649619cc8f0a30c7009c72d6023
dan-alvarosouza <dan.alvarosouza@gmail.com>
2024-11-16 12:20:31 -0300

    Created endpoint to fetch a list of all students

---
+++ StudentApi/APIs.cs
@@ -0,0 +1,13 @@
+﻿using StudentApi.Data;
+
+namespace StudentApi
+{
+	public static class APIs
+	{
+		public static void Register(WebApplication app)
+		{
+			app.MapGet("/students", (StudentContext context) => context.Students.ToArray());
+			app.MapGet("/students/{id}", (StudentContext context, int id) => context.Students.Find(id));
+		}
+	}
+}
--- StudentApi/Program.cs
+++ StudentApi/Program.cs
@@ -1,3 +1,4 @@
+using StudentApi;
 using StudentApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,26 +19,7 @@ if (app.Environment.IsDevelopment())
 }
 
 // app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+APIs.Register(app);
 
 app.Run();
 
